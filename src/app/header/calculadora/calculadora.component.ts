d59dee1b4389cde2196b003a1c586979a2bac29a
Saavedra98 <saavedraerazoc@gmail.com>
2022-12-27 11:12:42 -0500

    Actualizado

--- src/app/header/calculadora/calculadora.component.ts
+++ src/app/header/calculadora/calculadora.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  resultado: number = 0;
+  element = false;
 
+
+  operacion(num1: string, num2: string, operador:String){
+
+    if(operador=='+'){
+      this.resultado = Number(num1) + Number(num2);
+    }else if(operador=="-"){
+      this.resultado = Number(num1) - Number(num2);
+    }else if(operador=="*"){
+      this.resultado = Number(num1) * Number(num2);
+    }else{
+      this.resultado = Number(num1) / Number(num2);
+    }
+
+    return (this.element = true);
+
+    
+
+  }
 }
